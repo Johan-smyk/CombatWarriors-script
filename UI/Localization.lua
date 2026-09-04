6926d7e250a52888d3a12a3a5dce88f03c3cacc0
tuff <romapotakov@gmail.com>
2026-09-04 15:55:19 +0300

    Create Localization.lua

---
+++ UI/Localization.lua
@@ -0,0 +1,61 @@
+local Localization = {
+    English = {
+        AIMBOT = "AIMBOT",
+        SETTINGS = "SETTINGS",
+
+        Aimbot = "Aimbot",
+        Bows = "Bows",
+        Prediction = "Prediction",
+        FOV = "FOV",
+        FOVVisibility = "FOV Visibility",
+        RainbowFOV = "Rainbow FOV",
+        IgnorePlayers = "Ignore Players",
+
+        Language = "Language",
+        Theme = "Theme",
+
+        None = "None",
+        HeavyBow = "Heavy bow",
+        Crossbow = "Crossbow",
+        LongBow = "Long bow",
+
+        White = "White",
+        Black = "Black",
+        Gray = "Gray",
+
+        Ignored = "Ignored",
+        SearchPlayer = "Search player",
+        NoPlayers = "No players"
+    },
+
+    Русский = {
+        AIMBOT = "АИМБОТ",
+        SETTINGS = "НАСТРОЙКИ",
+
+        Aimbot = "Аимбот",
+        Bows = "Луки",
+        Prediction = "Предсказание",
+        FOV = "ФОВ",
+        FOVVisibility = "Видимость ФОВ",
+        RainbowFOV = "Радужный ФОВ",
+        IgnorePlayers = "Игнорировать игроков",
+
+        Language = "Язык",
+        Theme = "Тема",
+
+        None = "Нет",
+        HeavyBow = "Тяжёлый лук",
+        Crossbow = "Арбалет",
+        LongBow = "Длинный лук",
+
+        White = "Белая",
+        Black = "Чёрная",
+        Gray = "Серая",
+
+        Ignored = "Игнорируется",
+        SearchPlayer = "Поиск игрока",
+        NoPlayers = "Нет игроков"
+    }
+}
+
+return Localization
